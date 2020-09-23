fe97c2b18313df3e0ddd2e44637893e48d9a7895
A1esandr <ravulmirt@gmail.com>
2020-09-24 00:12:27 +0500

    Added simple echo implementation

---
+++ src/main/java/PollBot.java
@@ -0,0 +1,34 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.logging.Logger;
+
+public class PollBot extends TelegramLongPollingBot {
+    private static final Logger log = Logger.getLogger(PollBot.class.getName());
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            log.info(String.format("Get message: %s", update.getMessage().getText()));
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "PollBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return System.getenv("TOKEN");
+    }
+}

ebd0c9d57cec845afe0667d787b468400b5b2de6
A1esandr <ravulmirt@gmail.com>
2020-10-01 00:07:49 +0500

    Added QuizBot

--- src/test/java/QuizBotTest.java
+++ src/test/java/QuizBotTest.java
@@ -1,6 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.polls.SendPoll;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -8,38 +8,38 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-public class HiBotTest {
-    private HiBot hiBot;
+public class QuizBotTest {
+    private QuizBot quizBot;
 
     @Before
     public void setUp() {
-        hiBot = new HiBot("1");
+        quizBot = new QuizBot("1");
     }
 
     @Test
-    public void WhenHiBot_Construct_GetTokenFromArg() {
-        assertEquals("1", hiBot.getBotToken());
+    public void WhenQuizBot_Construct_GetTokenFromArg() {
+        assertEquals("1", quizBot.getBotToken());
     }
 
     @Test
-    public void WhenHiBot_Construct_SetName() {
-        assertEquals("HiBot", hiBot.getBotUsername());
+    public void WhenQuizBot_Construct_SetName() {
+        assertEquals("QuizBot", quizBot.getBotUsername());
     }
 
     @Test
-    public void WhenHiBot_ReceiveEmptyUpdate_NotCallExecute() {
-        HiBot bot = mock(HiBot.class);
+    public void WhenQuizBot_ReceiveEmptyUpdate_NotCallExecute() {
+        QuizBot bot = mock(QuizBot.class);
         Update update = new Update();
         doCallRealMethod().when(bot).onUpdateReceived(update);
 
         bot.onUpdateReceived(update);
 
-        verify(bot, times(0)).actualExecute(any(SendPhoto.class));
+        verify(bot, times(0)).actualExecute(any(SendPoll.class));
     }
 
     @Test
-    public void WhenHiBot_ReceiveNotEmptyUpdate_CallExecute() {
-        HiBot bot = mock(HiBot.class);
+    public void WhenQuizBot_ReceiveNotEmptyUpdate_CallExecute() {
+        QuizBot bot = mock(QuizBot.class);
         Update update = mock(Update.class);
         Message message = mock(Message.class);
         doReturn(true).when(update).hasMessage();
@@ -48,9 +48,10 @@ public class HiBotTest {
         doReturn(1L).when(message).getChatId();
         doReturn(message).when(update).getMessage();
         doCallRealMethod().when(bot).onUpdateReceived(update);
+        doCallRealMethod().when(bot).getSendPoll(update);
 
         bot.onUpdateReceived(update);
 
-        verify(bot, times(1)).actualExecute(any(SendPhoto.class));
+        verify(bot, times(1)).actualExecute(any(SendPoll.class));
     }
 }
